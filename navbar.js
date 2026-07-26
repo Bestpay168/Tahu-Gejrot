e4482992e5d94540ab7103e5542435ca49f4100b
Airdropcel <muhamadrivaioke@gmail.com>
2026-07-26 21:56:17 +0700

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,6 +1,24 @@
-const menuToggle = document.getElementById("menu-toggle");
+const burger = document.getElementById("burger");
 const navbar = document.getElementById("navbar");
+const overlay = document.getElementById("overlay");
 
-menuToggle.addEventListener("click", function () {
+burger.onclick = () => {
+
     navbar.classList.toggle("active");
-});
+    overlay.classList.toggle("show");
+
+    if(navbar.classList.contains("active")){
+        burger.innerHTML = "✕";
+    }else{
+        burger.innerHTML = "☰";
+    }
+
+};
+
+overlay.onclick = () => {
+
+    navbar.classList.remove("active");
+    overlay.classList.remove("show");
+    burger.innerHTML = "☰";
+
+};
